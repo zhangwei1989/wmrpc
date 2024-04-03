1f8804f345e410bc42729dbecd519b723d9f88a9
zhangwei <zhangwei@19890518@gmail.com>
2024-04-03 17:23:00 +0800

    add service metas

--- wmrpc-core/src/main/java/cn/william/wmrpc/core/meta/ServiceMeta.java
+++ wmrpc-core/src/main/java/cn/william/wmrpc/core/meta/ServiceMeta.java
@@ -1,9 +1,13 @@
 package cn.william.wmrpc.core.meta;
 
+import com.alibaba.fastjson.JSON;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 描述服务元数据
@@ -25,7 +29,13 @@ public class ServiceMeta {
 
     private String name;
 
+    private Map<String, String> parameters = new HashMap<>();
+
     public String toPath() {
         return String.format("%s_%s_%s_%s", app, namespace, env, name);
+    }
+
+    public String toMetas() {
+        return JSON.toJSONString(this.getParameters());
     }
 }
--- wmrpc-core/src/main/java/cn/william/wmrpc/core/registry/zk/ZkRegistryCenter.java
+++ wmrpc-core/src/main/java/cn/william/wmrpc/core/registry/zk/ZkRegistryCenter.java
@@ -62,7 +62,7 @@ public class ZkRegistryCenter implements RegistryCenter {
         try {
             // 创建服务的持久化节点
             if (client.checkExists().forPath(servicePath) == null) {
-                client.create().withMode(CreateMode.PERSISTENT).forPath(servicePath, "service".getBytes());
+                client.create().withMode(CreateMode.PERSISTENT).forPath(servicePath, service.toMetas().getBytes());
             }
             // 创建实例的临时性节点
             String instancePath = servicePath + "/" + instanceMeta.toPath();
